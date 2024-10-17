c4a40c1b45f9a1e21f62306d8ca396a51ee18adb
erickjllo <erickjllo07@gmail.com>
2024-10-17 12:45:30 -0500

    initial

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -1,1 +1,11 @@
-# TODO: Implementa el código del ejercicio aquí
+from abc import ABC
+
+class ReglaValidacion(ABC):
+    def _validar_longitud():
+        pass
+    def _contiene_mayuscula():
+        pass
+    def _contiene_minuscula():
+        pass
+    def _contiene_numero():
+        pass
