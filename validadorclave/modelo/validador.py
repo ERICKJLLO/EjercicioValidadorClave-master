daa20c4a423a2aaa7d046bf37147eac18d05c2e4
erickjllo <erickjllo07@gmail.com>
2024-10-17 12:59:27 -0500

    edit functions of class ReglaValidacion

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -1,11 +1,21 @@
 from abc import ABC
 
 class ReglaValidacion(ABC):
-    def _validar_longitud():
+    def __init__(self, longitud_minima=8):
+        self.longitud_minima = longitud_minima
+
+    @abstractmethod
+    def es_valida(self, clave):
         pass
-    def _contiene_mayuscula():
-        pass
-    def _contiene_minuscula():
-        pass
-    def _contiene_numero():
-        pass
+
+    def _contiene_mayuscula(self, clave):
+        return any(c.isupper() for c in clave)
+
+    def _contiene_minuscula(self, clave):
+        return any(c.islower() for c in clave)
+
+    def _contiene_numero(self, clave):
+        return any(c.isdigit() for c in clave)
+
+    def _validar_longitud(self, clave):
+        return len(clave) >= self.longitud_minima
