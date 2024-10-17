40b6da7db7176a42f6209ecf9e8629b03507d9cf
erickjllo <erickjllo07@gmail.com>
2024-10-17 13:13:43 -0500

    create class ReglaValidacionGanimedes

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -1,4 +1,5 @@
-from abc import ABC
+from abc import ABC, abstractmethod
+from errores import ValidadorError, NoCumpleLongitudMinimaError, NoTieneCaracterEspecialError, NoTieneLetraMayusculaError, NoTieneLetraMinusculaError, NoTieneNumeroError, NoTienePalabraSecretaError
 
 class ReglaValidacion(ABC):
     def __init__(self, longitud_minima=8):
@@ -19,3 +20,24 @@ class ReglaValidacion(ABC):
 
     def _validar_longitud(self, clave):
         return len(clave) >= self.longitud_minima
+    
+class ReglaValidacionGanimedes(ReglaValidacion):
+    def __init__(self):
+        super().__init__(longitud_minima=8)
+
+    def contiene_caracter_especial(self, clave):
+        return bool(re.search(r'[!@#$%^&*()_+=\-{}\[\]:;"\'<>,.?/]', clave))
+
+    def es_valida(self, clave):
+        if not self._validar_longitud(clave):
+            raise NoCumpleLongitudMinimaError
+        if not self._contiene_mayuscula(clave):
+            raise NoTieneLetraMayusculaError
+        if not self._contiene_minuscula(clave):
+            raise NoTieneLetraMinusculaError
+        if not self._contiene_numero(clave):
+            raise NoTieneNumeroError
+        if not self.contiene_caracter_especial(clave):
+            raise NoTieneCaracterEspecialError
+
+        return True
